dcf9cce3ccaa3af74b988a536348e0124eee874c
VADER1998 <37174770+VADER1998@users.noreply.github.com>
2019-04-27 12:14:01 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# SocialCard
-Created with CodeSandbox
---
+++ src/card.js
@@ -0,0 +1,16 @@
+import React  from 'react';
+
+function Card(){
+    return(
+      <div className="card">
+      <div class="container">
+          <h3>Want to know which mobile phone to choose?</h3>
+       </div>
+       <div class="rest">
+           <p>killMonger66</p>
+          </div>
+      </div>
+    )
+  }
+
+export default Card;
